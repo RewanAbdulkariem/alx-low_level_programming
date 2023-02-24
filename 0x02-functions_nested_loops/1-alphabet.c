0059d4c2027f95c89531a4a33fd61d66f615ea2f
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-02-24 09:02:03 +0200

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * Write a function that prints the alphabet, in lowercase
+ * , followed by a new line.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char alpha;
+	for (alpha = 97; alpha <= 122; alpha++)
+	{
+		_putchar(alpha);
+	}
+	_putchar('\n');
+	return (0);
+}
