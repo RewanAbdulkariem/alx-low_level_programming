bfcf8f263b4ec0a791c0594df7d24674afc8a6c1
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-02-24 12:10:36 +0200

    advanced

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -35,6 +35,6 @@ void print_times_table(int n)
 					_putchar((k / 100) + '0');
 					_putchar(((k / 10) % 10) + '0');
 					_putchar((k % 10) + '0'); } 
-			j != n ? _putchar(','):_putchar(' ') ; }
+			j != n ? _putchar(','):_putchar('\0'); }
 			_putchar('\n');
 }}}
