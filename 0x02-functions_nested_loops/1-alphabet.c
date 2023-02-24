08df36577fda21ce9942d050b320484bd7d362f6
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-02-24 09:07:41 +0200

    fix an error

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,11 +4,11 @@
  * Write a function that prints the alphabet, in lowercase
  * , followed by a new line.
  *
- * Return: Always 0.
  */
 void print_alphabet(void)
 {
 	char alpha;
+
 	for (alpha = 97; alpha <= 122; alpha++)
 	{
 		_putchar(alpha);
