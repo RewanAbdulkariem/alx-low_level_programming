7b0243756ec79e028aade58d2c8796f7e233e49f
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-03-02 09:15:17 +0200

    task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *mul - Multiply 2 integers
+ *@a: first integer
+ *@b: second integer
+ *Return: Return the result of multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,4 +3,5 @@
 extern int _putchar(char c);
 extern int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 #endif
