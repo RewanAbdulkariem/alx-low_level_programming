3668fabebf9c22c744a23bd6a48f82b0fbedfd99
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-03-02 13:41:19 +0200

    task5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * rev_string - a string, in reverse,
+ * @s: the string to revese
+ */
+void rev_string(char *s)
+{
+	int i;
+	int length = 0;
+	char *r;
+
+	for (length = 0; s[length] != '\0'; length++);
+	for (i = length - 1; i >= 0; i--)
+	{
+		r[length-1-i] = s[i];
+	}
+	*s = *r;
+}
