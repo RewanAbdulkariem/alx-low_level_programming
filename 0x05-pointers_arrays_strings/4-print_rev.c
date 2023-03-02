f1f0c33043473bda58d84d4162b12781d9adac72
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-03-02 13:15:50 +0200

    task3

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse,
+ * @s: the string to revese
+ */
+void print_rev(char *s);
+{
+	int i;
+	char *r;
+
+	int length = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++;
+	}
+
+	for (i = length - 1; i < 0; i++)
+	{
+		_putchar(r[i]);
+	}
+	_putchar('\n');
+}
