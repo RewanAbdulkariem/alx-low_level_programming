690248787f7329927003d8b25898d98f72433dd1
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-03-02 10:06:08 +0200

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,14 +8,14 @@
  */
 void print_line(int n)
 {
-        int i;
-
-        i = 0;
-
-        while (i < n)
-        {
-                _putchar('_');
-                i++;
-        }
-        _putchar('\n');
+	int i;
+        
+	i = 0;
+        
+	while (i < n)
+	{
+	_putchar('_');
+	i++;
+	}
+	_putchar('\n');
 }
