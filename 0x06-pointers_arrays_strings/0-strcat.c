1ee09d4e9607fb3039dd3e575703061c6aa01e4b
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-03-02 18:10:49 +0200

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,11 +12,12 @@ char *_strcat(char *dest, char *src)
 	int l1 = 0, l2 = 0;
 
 	while ( dest[l1++] != '\0')
-	{	}
+	{	
+	}
 
-	for (l2 = 0; src[l2] != '\0' ;l2++,l1++)
+	for (l2 = 0; src[l2] != '\0'; l2++, l1++)
 	{
-		dest[l1-1] = src[l2];
+		dest[l1 - 1] = src[l2];
 	}
 	dest[l1] = '\0';
 	return (dest);
