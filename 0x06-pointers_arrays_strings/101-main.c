d02ffdc677f60124c75800a62165abb0a18571fc
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-03-03 22:04:38 +0200

    correct some errors

---
+++ 0x06-pointers_arrays_strings/101-main.c
@@ -0,0 +1,22 @@
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+ extern int _putchar(char c);
+void print_number(int n);
+int main(void)
+{
+    print_number(98);
+    _putchar('\n');
+    print_number(402);
+    _putchar('\n');
+    print_number(1024);
+    _putchar('\n');
+    print_number(0);
+    _putchar('\n');
+    print_number(-98);
+    _putchar('\n');
+    return (0);
+}
