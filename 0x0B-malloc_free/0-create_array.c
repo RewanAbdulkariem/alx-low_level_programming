67ed8dabb5194c471ff52e7a94df0f821e8118ca
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-03-13 22:40:16 +0200

    task1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,21 +7,21 @@
  *
  * Return: pointer of an array of chars
  */
- char *create_array(unsigned int size, char c)
- {
- 	char* arr;
- 	unsigned int i = 0;
- 	
- 	if (size == 0)
- 		return (NULL);
+char *create_array(unsigned int size, char c)
+{
+	char* arr;
+	unsigned int i = 0;
 
- 	arr = malloc(sizeof(char) * size);
- 	
- 	if (arr == NULL)
- 		return (NULL);
- 	
+	if (size == 0)
+		return (NULL);
+
+	arr = malloc(sizeof(char) * size);
+
+	if (arr == NULL)
+		return (NULL);
+
 	for (i = 0; i < size; i++)
 		arr[i] = c;
-	
+
 	return (arr);
- }
+}
