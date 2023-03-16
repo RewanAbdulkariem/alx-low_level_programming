6b203b09dbed629b3b3e5ab94a5c9cc070f5c0e0
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-03-16 12:01:05 +0200

    task5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - main funct
+ * @n: int n
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return -1;
+	if (n == 0)
+		return 0;
+
+	return sqrt_helper(n, 1, n / 2);
+}
+/**
+ * sqrt_helper - main funct
+ * @n: int n
+ * @start: start
+ * @end: end
+ * Return: int
+ */
+int sqrt_helper(int n, int start, int end) 
+{
+	int mid, mid_squared;
+	if (start > end)
+		return -1; 
+	mid = start + (end - start) / 2;
+	mid_squared = mid * mid;
+
+	if (mid_squared == n)
+		return mid;
+	if (mid_squared < n)
+		return sqrt_helper(n, mid + 1, end);
+
+	return sqrt_helper(n, start, mid - 1);
+}
