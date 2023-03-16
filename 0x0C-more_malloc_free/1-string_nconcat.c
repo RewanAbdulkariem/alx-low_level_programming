93d27e29040ad1ea355d543bd26d759dd63e8240
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-03-16 15:19:52 +0200

    task0

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,29 +21,29 @@ unsigned int _strlen(char *string)
  * Return: pointer to concatenated string
  * If the function fails, it should return NULL
  */
- char *string_nconcat(char *s1, char *s2, unsigned int n)
- {
- 	char *s3;
- 	unsigned int i, j, len;
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *s3;
+	unsigned int i, j, len;
 
- 	if (!s1)
- 		s1 = " ";
- 	if (!s2)
- 		s2 = " ";
- 	if (n >= _strlen(s2))
+	if (!s1)
+		s1 = " ";
+	if (!s2)
+		s2 = " ";
+	if (n >= _strlen(s2))
 		n = _strlen(s2);
 
- 	len =  _strlen(s1) + n;
- 	s3 = malloc(len * sizeof(char));
+	len =  _strlen(s1) + n;
+	s3 = malloc(len * sizeof(char));
 
- 	if (!s3)
- 		return (NULL);
+	if (!s3)
+		return (NULL);
 
- 	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 		s3[i] = s1[i];
 	for (j = 0; j < n; j++)
 		s3[i + j] = s2[j];
 	s3[i + j] = '\0';
 
 	return (s3);
- }
+}
