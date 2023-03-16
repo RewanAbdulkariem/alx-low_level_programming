64e886099826eed5fc10b3b573a560987df79b80
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-03-16 15:12:36 +0200

    add tak1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,26 +25,24 @@ unsigned int _strlen(char *string)
  {
  	char *s3;
  	unsigned int i, j, len;
- 	
+
  	if (!s1)
  		s1 = " ";
  	if (!s2)
  		s2 = " ";
  	if (n >= _strlen(s2))
 		n = _strlen(s2);
- 	
+
  	len =  _strlen(s1) + n;
  	s3 = malloc(len * sizeof(char));
  	if (!s3)
  		return (NULL);
- 	
+
  	for (i = 0; s1[i] != '\0'; i++)
 		s3[i] = s1[i];
 	for (j = 0; j < n; j++)
 		s3[i + j] = s2[j];
 	s3[i + j] = '\0';
 
-	return (s3); 
- 	
- 	
+	return (s3);
  }
