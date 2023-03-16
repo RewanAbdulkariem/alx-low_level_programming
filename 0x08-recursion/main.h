1a14b2e0bba5777829c8e9e7da6754f48754d8e2
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-03-16 13:55:45 +0200

    tasks

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,5 +8,11 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int _sqrt(int n, int start, int end);
+int divisors(int n, int m);
+int is_prime_number(int n);
+int _strlen_recursion(char *s);
+int pand(char *s, int l);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 #endif
 
