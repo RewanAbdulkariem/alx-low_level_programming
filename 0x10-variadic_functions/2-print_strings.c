f4c0310dd8507add61fba9ff27fb2104f3b3a981
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-03-23 20:33:18 +0200

    add task 4

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,12 +1,12 @@
 #include "variadic_functions.h"
-
+#include <stdarg.h>
+#include <stdio.h>
 /**
  * print_strings - Print numbers using a separator
- * @separator: The string separator
- * @n: The quantity of numbers
+ * @separator: the string to be printed between the strings
+ * @n: the number of strings passed to the function
  * @...: The list of numbers
  *
- * Return: Nothing
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
