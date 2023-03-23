e47679fe564a2f41024f932c14667c3a38cb0678
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-03-23 20:04:22 +0200

    add first task

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+* sum_them_all - sum of all its parameters
+* @n: n args
+* Return: sum or if n == 0, return 0
+*/
+int sum_them_all(const unsigned int num_args, ...)
+{
+	int sum = 0;
+	unsigned int i;
+	va_list args;
+
+	if (num_args)
+	{
+		va_start(args, num_args);
+		for (i = 0; i < num_args; i++)
+		{
+			sum += va_arg(args, int);
+		}
+		va_end(args);
+	}
+	return (sum);
+}
