92089a1d78acb9d4eed34980efd4a9d4e4477422
Rewan Khaled <83522163+RewanAbdulkariem@users.noreply.github.com>
2023-03-27 17:40:47 +0200

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,13 @@
 #include <stddef.h>
 /**
- * print_name - take name and function 
- * and pass name to function
- * @name: name of the person
- * @f: pointer to a function
- * Return: void
- */
+* print_name - take name and function 
+* and pass name to function
+* @name: name of the person
+* @f: pointer to a function
+* Return: void
+*/
 void print_name(char *name, void (*f)(char *))
 {
 	if (f != NULL)
-	f(name);
+		f(name);
 }
