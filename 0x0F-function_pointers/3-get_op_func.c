98436cdec05f923172914be2f9dfb70079c4e086
Rewan Khaled <83522163+RewanAbdulkariem@users.noreply.github.com>
2023-03-27 17:50:19 +0200

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -17,13 +17,13 @@ int (*get_op_func(char *s))(int, int)
 	{"%", op_mod},
 	{NULL, NULL}
 	};
-	
+
 	int i = 0;
-	
+
 	while (i < 5)
 	{
 		if (!strcmp(s, ops[i].op))
-			return (ops[i].f);	
+			return (ops[i].f);
 		i++;
 	}
 
