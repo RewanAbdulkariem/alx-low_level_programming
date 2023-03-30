7549a6744b9517cc3893df8b67d17fb3e6c3eacb
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-03-30 19:00:25 +0200

    edit task1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,24 +1,25 @@
-#include"lists.h"
+#include <stdio.h>
+#include "lists.h"
+
 /**
- * print_list - prints all the elements of a list_t
- * @h: list
- * Return: the number of nodes
+ * print_list - prints all the elements of a linked list
+ * @h: pointer to the list_t list to print
+ * Return: the number of nodes printed
  */
- 
 size_t print_list(const list_t *h)
 {
-	size_t n;
+	size_t n = 0;
 
- 	n = 0;
-
-	while (h != NULL)
+	while (h)
 	{
 		if (!h->str)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
-		n ++;
 		h = h->next;
+		n++;
 	}
+
 	return (n);
 }
+
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,8 +2,6 @@
 #define LISTS_H
 #include <stddef.h>
 #include <stdio.h>
-size_t print_list(const list_t *h);
-int _putchar(char c);
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -18,4 +16,6 @@ typedef struct list_s
     unsigned int len;
     struct list_s *next;
 } list_t;
+size_t print_list(const list_t *h);
+int _putchar(char c);
 #endif
