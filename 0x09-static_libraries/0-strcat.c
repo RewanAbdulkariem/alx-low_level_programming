78c595c9859084bb1d96950cc001cb5405c16972
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-07-22 18:52:22 +0300

    task 8

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: a string to be concatinated to
+ * @src: a string to concatinate
+ * Return: a pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int l1, l2;
+
+	l1 = 0;
+	l2 = 0;
+
+	while (dest[l1++] != '\0')
+	{
+	}
+
+	for (l2 = 0; src[l2] != '\0'; l2++, l1++)
+	{
+		dest[l1 - 1] = src[l2];
+	}
+	dest[l1] = '\0';
+	return (dest);
+}
