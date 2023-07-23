bb32dca1ef20d7117d30f5cab884e9481de711db
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-07-23 23:46:53 +0300

    Betty coding style

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
-		return(NULL);
+		return (NULL);
 
 	arr = malloc(nmemb * size);
 	if (arr == NULL)
