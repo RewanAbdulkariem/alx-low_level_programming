cfa3e1b61bf1d44c528ce81cbbaba53b25d1667f
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-07-26 22:49:33 +0300

    add description

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,11 +14,11 @@ void print_dog(struct dog *d)
 	{
 		if (d->name)
 		{
-		    name = d->name;
+			name = d->name;
 		}
 		if (d->owner)
 		{
-		    owner = d->owner;
+			owner = d->owner;
 		}
 		printf("Name: %s\nAge: %f\nOwner: %s\n", name, d->age, owner);
 	}
