39142368a46830add5e6b7ada86fed59632b4a50
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-07-26 22:18:25 +0300

    add description

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,7 +1,11 @@
 #include "dog.h"
 
 /**
- *
+ * init_dog - initalize dog
+ *@d: dog structue
+ *@name: dog name
+ *@age: dog age
+ *@owner: dog owner
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
