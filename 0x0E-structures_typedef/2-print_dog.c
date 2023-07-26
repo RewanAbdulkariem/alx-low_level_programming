eec51dcb75135cacedaaef029a2ded5aa1ce518f
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-07-26 22:41:23 +0300

    task

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ *
+ */
+void print_dog(struct dog *d)
+{
+	char *name = "nil";
+	char *owner = "nil";
+
+	if (d)
+	{
+		if (d->name)
+		    name = d->name;
+		if (d->owner)
+		    owner = d->owner;
+		printf("Name: %s\nAge: %f\nOwner: %s\n", name, d->age, owner);
+	}
+}
