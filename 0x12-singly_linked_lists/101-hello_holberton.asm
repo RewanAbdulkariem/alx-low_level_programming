66852358a35811755be47a3a6975c56c0b58063f
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-07-28 21:00:20 +0300

    add new line

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -7,10 +7,17 @@ main:
 	mov       rsi, message            ; address of string to output
 	mov       rdx, 17                 ; number of bytes
 	syscall                           ; invoke operating system to do the write
+	
+	mov       rax, 1                  ; system call for write
+	mov       rdi, 1                  ; file handle 1 is stdout
+	mov       rsi, newline            ; address of string to output
+	mov       rdx, 1                 ; number of bytes
+	syscall                           ; invoke operating system to do the write
+	
 	mov       rax, 60                 ; system call for exit
 	xor       rdi, rdi                ; exit code 0
 	syscall                           ; invoke operating system to exit
 
 	section   .data
 message:  db        "Hello, Holberton", 0  ; note the newline at the end
-
+newline:  db 10                   ; newline character ('\n')
