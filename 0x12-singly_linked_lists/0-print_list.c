3cc304f01674c5596c9428ec2e74c27492de129d
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-07-28 18:58:34 +0300

    fix betty style

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,16 +8,16 @@ size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
 	int i;
-	
+
 	const list_t *currentNode = h;
-	
-	for(i = 0; currentNode != NULL; i++)
+
+	for (i = 0; currentNode != NULL; i++)
 	{
 		if (currentNode->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n",currentNode->len , currentNode->str);
-		currentNode = currentNode -> next;
+			printf("[%d] %s\n", currentNode->len, currentNode->str);
+		currentNode = currentNode->next;
 	}
 	nodes = i;
 	return (nodes);
