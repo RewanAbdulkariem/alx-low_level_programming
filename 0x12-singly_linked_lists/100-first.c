382700f116e3e0d286db4ec4ad305071eda95745
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-07-28 19:50:58 +0300

    some startup code can be executed before main() starts

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+
+void myStartupFun (void) __attribute__ ((constructor));
+
+void myStartupFun()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
