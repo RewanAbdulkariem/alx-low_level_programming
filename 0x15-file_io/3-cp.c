084472817929798cba54be99ec1b9d9a72b83ad5
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-08-09 06:53:50 +0300

    task cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * main - program that copies the content of a file to another
- * @argc: number of argument
+ * @argc: number of arguments
  * @argv: arguments
- * Return: 0
+ * Return: 0 on success, other values on error
  */
 int main(int argc, char *argv[])
 {
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 			ErrorClosing(file_from);
 		ErrorWriting(argv[2]);
 	}
-	do{
+	do {
 		n = read(file_from, buf, 1024);
 		if (n < 0)
 		{
@@ -39,7 +39,7 @@ int main(int argc, char *argv[])
 				ErrorClosing(file_to);
 			ErrorReading(argv[1]);
 		}
-		
+
 		if (write(file_to, buf, n) < 0)
 		{
 			if (close(file_from) < 0)
@@ -49,25 +49,36 @@ int main(int argc, char *argv[])
 			ErrorWriting(argv[2]);
 		}
 	} while (n != 0);
-	
+
 	if (close(file_from) < 0)
 		ErrorClosing(file_from);
 	if (close(file_to) < 0)
 		ErrorClosing(file_to);
-	
+
 	return (0);
 }
-
+/**
+ * ErrorReading - Print error message and exit due to read error
+ * @file_name: name of the file
+ */
 void ErrorReading(char *file_name)
 {
 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_name);
 	exit(98);
 }
+/**
+ * ErrorClosing - Print error message and exit due to file close error
+ * @file: file descriptor
+ */
 void ErrorClosing(int file)
 {
 	dprintf(STDERR_FILENO, "Error: Can't close fd %d", file);
 	exit(100);
 }
+/**
+ * ErrorWriting - Print error message and exit due to write error
+ * @file_name: name of the file
+ */
 void ErrorWriting(char *file_name)
 {
 	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_name);
