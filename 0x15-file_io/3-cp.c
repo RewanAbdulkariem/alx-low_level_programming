565211a9f4dc8dc0a3148a70e3075ebec1207594
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-08-09 06:19:19 +0300

    solve reading file till the end

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * main - program that copies the content of a file to another
+ * @argc: number of argument
+ * @argv: arguments
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
 	int file_to, file_from, n;
@@ -14,62 +19,57 @@ int main(int argc, char *argv[])
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		ErrorReading(argv[1]);
 	}
 
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_to < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		if (close(file_from) < 0)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d", file_from);
-			exit(100);
-		}
-		exit(99);
+			ErrorClosing(file_from);
+		ErrorWriting(argv[2]);
 	}
-	n = read(file_from, buf, 1024);
-	if (n < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		if (close(file_from) < 0)
+	do{
+		n = read(file_from, buf, 1024);
+		if (n < 0)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d", file_from);
-			exit(100);
+			if (close(file_from) < 0)
+				ErrorClosing(file_from);
+			if (close(file_to) < 0)
+				ErrorClosing(file_to);
+			ErrorReading(argv[1]);
 		}
-		if (close(file_to) < 0)
+		
+		if (write(file_to, buf, n) < 0)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d", file_to);
-			exit(100);
+			if (close(file_from) < 0)
+				ErrorClosing(file_from);
+			if (close(file_to) < 0)
+				ErrorClosing(file_to);
+			ErrorWriting(argv[2]);
 		}
-		exit(98);
-	}
+	} while (n != 0);
 	
-	if (write(file_to, buf, n) < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		if (close(file_from) < 0)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d", file_from);
-			exit(100);
-		}
-		if (close(file_to) < 0)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d", file_to);
-			exit(100);
-		}
-		exit(99);
-	}
 	if (close(file_from) < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", file_from);
-		exit(100);
-	}
+		ErrorClosing(file_from);
 	if (close(file_to) < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", file_to);
-		exit(100);
-	}
+		ErrorClosing(file_to);
+	
 	return (0);
+}
+
+void ErrorReading(char *file_name)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_name);
+	exit(98);
+}
+void ErrorClosing(int file)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d", file);
+	exit(100);
+}
+void ErrorWriting(char *file_name)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_name);
+	exit(99);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,4 +11,7 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void ErrorReading(char *file_name);
+void ErrorClosing(int file);
+void ErrorWriting(char *file_name);
 #endif
