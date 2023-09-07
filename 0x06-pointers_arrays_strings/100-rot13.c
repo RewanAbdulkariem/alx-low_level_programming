edd3e974364e8cb634a8ff722a692817e0318593
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-09-07 06:07:00 +0300

    advanced

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * rot13 -  a   function that encodes a string using rot13.
@@ -7,21 +8,19 @@
  */
 char *rot13(char *s)
 {
-	int i = 0;
+	int i;
 
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		while ((s[i] >= 'a' && s[i] <= 'z') ||
-				(s[i] >= 'A' && s[i] <= 'Z'))
+		while ((s[i] >= 65 && s[i] <= 77) || (s[i] >= 97 && s[i] <= 109))
 		{
-			if ((s[i] >= 'a' && s[i] <= 'm') ||
-					(s[i] >= 'A' && s[i] <= 'M'))
-				s[i] += 13;
-			else
-				s[i] -= 13;
+			s[i] += 13;
 			i++;
 		}
-		i++;
+		if ((s[i] >= 78 && s[i] <= 90) || (s[i] >= 110 && s[i] <= 122))
+		{
+			s[i] -= 13;
+		}
 	}
 	return (s);
 }
