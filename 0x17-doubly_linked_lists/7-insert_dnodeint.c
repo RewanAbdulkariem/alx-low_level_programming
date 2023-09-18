c3b69a379a831919c16dac7483238419f552b42a
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-09-18 05:51:37 +0300

    fix betty

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,7 +22,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = NULL;
-    
+
 	if (idx == 0)
 	{
 		return (add_dnodeint(h, n));
