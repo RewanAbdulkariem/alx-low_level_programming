d63b6c7e6e4e11cd914f207f8cd337c66901a843
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-09-18 05:33:42 +0300

    fix idx 0

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,18 +16,33 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+
+	if (idx == 0)
+	{
+		return (add_dnodeint(h, n));
+	}
+
 	current = *h;
 	for (i = 0; i < idx - 1 && current != NULL; i++)
 	{
 		current = current->next;
 	}
 	if (i < idx - 1)
+	{
+		free(new_node);
 		return (NULL);
+	}
+
 	new_node->next = current->next;
+	new_node->prev = current;
+
 	current->next->prev = new_node;
 	current->next = new_node;
-	new_node->prev = current;
-	new_node->n = n;
+	
 
 	return (new_node);
 }
