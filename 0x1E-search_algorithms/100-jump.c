930f759fbc724fb156e9eb7740e9716de5dcecb0
RewanAbdulkariem <rewan.khaled21@gmail.com>
2024-07-16 19:41:27 +0300

    .

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -38,7 +38,7 @@ int jump_search(int *array, size_t size, int value)
 
 	for (i = start; i <= end; i++)
 	{
-		printf("checked array[%ld] = [%d]\n", i, array[i]);
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
