c02ced176210e69fdb4609852667e86d1c0d64f3
RewanAbdulkariem <rewan.khaled21@gmail.com>
2024-07-17 01:05:40 +0300

    fix

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -40,6 +40,6 @@ int interpolation_search(int *array, size_t size, int value)
 	pos = low + (((double)(high - low) /
 		(array[high] - array[low])) * (value - array[low]));
 
-	printf("checked array[%lu] is out of range\n", pos);
+	printf("Value checked array[%lu] is out of range\n", pos);
 	return (-1);
 }
