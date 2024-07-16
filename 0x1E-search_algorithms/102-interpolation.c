522dd83a6035b308e5370c1e1d44e88f0959ffd2
RewanAbdulkariem <rewan.khaled21@gmail.com>
2024-07-17 01:38:06 +0300

    fix

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -25,7 +25,7 @@ int interpolation_search(int *array, size_t size, int value)
 		{
 			if (array[low] == value)
 				return (low);
-			return (-1);
+			break;
 		}
 		pos = low + (((double)(high - low) /
 		(array[high] - array[low])) * (value - array[low]));
