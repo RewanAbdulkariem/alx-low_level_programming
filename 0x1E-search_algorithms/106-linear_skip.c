e2fc94b5b48acab088207d6471dfab213013ce30
RewanAbdulkariem <rewan.khaled21@gmail.com>
2024-07-17 22:21:17 +0300

    107

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <math.h>
+#include "search_algos.h"
+
+/**
+ * linear_skip - searches for a value in a sorted skip list of integers.
+ * @list: a pointer to the head of the skip list to search in
+ * @size: the number of nodes in list
+ * @value: the value to search for
+ * Return: a pointer to the first node where value is located
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+    
+}
